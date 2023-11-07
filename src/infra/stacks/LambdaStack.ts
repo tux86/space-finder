67f951ebc362cf1d02d4b1c8f26c6c41b3f2aded
Walid Karray <walid.karray@gmail.com>
2023-11-07 18:04:03 +0100

    add api gw

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -2,16 +2,23 @@ import {Stack, StackProps} from "aws-cdk-lib";
 import {Construct} from "constructs";
 import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
 import {join} from "path"
+import {LambdaIntegration} from "aws-cdk-lib/aws-apigateway";
 export class LambdaStack extends Stack {
+
+
+    public readonly helloLambdaIntegration: LambdaIntegration
+
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
 
-        new Function(this,'HelloLamda', {
+        const helloLambda = new Function(this,'HelloLambda', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'hello.main',
             code: Code.fromAsset(join(__dirname, '..','..', 'services'))
         })
+
+        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
 
     }
 }
