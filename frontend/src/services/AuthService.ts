cf4d0c72264aef7bc0d10158d76144322fc85bd9
Walid Karray <walid.karray@gmail.com>
2023-11-10 11:50:01 +0100

    wip

---
+++ frontend/src/services/AuthService.ts
@@ -0,0 +1,39 @@
+import {AuthStack} from "../../../infra/outputs.json"
+import {Amplify, Auth} from "aws-amplify";
+import {CognitoUser} from "@aws-amplify/auth";
+
+const awsRegion = 'eu-west-1';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: false,
+        region: awsRegion,
+        userPoolId: AuthStack.SpaceUserPoolId,
+        userPoolWebClientId: AuthStack.SpaceUserPoolClientId,
+        identityPoolId: AuthStack.SpaceIdentityPoolId,
+        authenticationFlowType: 'USER_PASSWORD_AUTH'
+    }
+})
+
+
+export class AuthService {
+
+
+    private user: CognitoUser | undefined;
+
+    public async login(userName: string, password: string): Promise<Object | undefined> {
+
+        try {
+            this.user = await Auth.signIn(userName, password) as CognitoUser;
+            return this.user;
+        } catch (error) {
+            console.error(error);
+            return undefined
+        }
+
+    }
+
+    public getUserName() {
+        return this.user?.getUsername();
+    }
+}
